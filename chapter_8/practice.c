8b3c622c325bedf056c2f37d701af3dfc774ce50
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2025-10-01 16:28:59 +0600

    till now

--- chapter_8/practice.c
+++ chapter_8/practice.c
@@ -35,3 +35,95 @@ int countLength(char arr[]) {
     }
     return count-1;
 }
+
+//take input using %c
+int main () {
+    char str[100];
+    char ch;
+    int i = 0;
+
+    while (ch != '\n') {
+        scanf("%c", &ch);
+        str[i] = ch;
+        i++; 
+    }
+    str[i] = '\0';
+    puts(str);
+}
+
+//salting
+void salting(char pass[]);
+
+int main() {
+    char pass[100];
+    scanf("%s", pass);
+    salting(pass);
+}
+
+coid salting(char pass[]) {
+    char salt[] = "123";
+    char newPass[200];
+
+    strcpy(newPass, pass);
+    strcat(newPass, salt);
+    puts(newPass);
+}
+
+//slice
+void slice(char str[], int n, int m,);
+
+int main() {
+    char str[] = "helloworld";
+    slice(str, 3, 6);
+}
+
+void slice(char str[], int n, int m,) {
+    char newStr[200];
+    int j = 0;
+    for(int i=n; j=0; i<=m; i++, j++) {
+        newStr[j] = str[i];
+    }
+    newStr[i] = '\0';
+    puts(newStr);
+}
+
+//count vowel
+int countVowel(char str[]);
+
+int main () {
+    char str[] = "hello world";
+    printf("%d", countVowel(str));
+}
+
+int countVowel(char str[]) {
+    int count = 0;
+
+    for(int i=0; str[i] != '\0'; i++) {
+        if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u') {
+            count ++;
+
+        }
+    }
+    return 0;
+}
+
+//check character
+void checkChar(char str[], char ch);
+
+int main () {
+    char str[] = "hello world";
+    char ch = 'e';
+checkChar(str, ch);
+}
+
+void checkChar(char str[], char ch) {
+
+    for(int i=0; str[i] != '\0'; i++) {
+        if(str[i] == ch) {
+            printf("present");
+            return;
+        }
+
+        }
+        printf("not present");
+    }
